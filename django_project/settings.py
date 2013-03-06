290fe7b7b6aa03060a73f46d3328ad8f5a4699c0
Michael Vogt <mvo@ubuntu.com>
2013-03-06 09:04:13 +0100

    add a production config

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,13 +38,18 @@ MANAGERS = ADMINS
 LOGIN_URL = "/accounts/login/"
 
 DATABASES = {
-    'default': {
+    'production': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'fwadmin.db',                # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        
+    },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'fwadmin.db',
     }
 }
 
---
+++ django_project/settings_production.py
@@ -0,0 +1,6 @@
+from settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+DATABASE_NAME = "production"
